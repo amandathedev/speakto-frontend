15d2157d26352f818d6dffd65a513fcc39e9438a
Amanda Treutler <amandathedev@gmail.com>
2019-09-20 21:43:13 -0500

    adds forms

--- src/components/StudentComponents/StudentRegistration.js
+++ src/components/StudentComponents/StudentRegistration.js
@@ -2,100 +2,138 @@ import React, { Component } from "react";
 import "../../styles/Registration.css";
 
 export default class Registration extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      username: "",
+      email: "",
+      password_digest: "",
+      skype_id: "",
+      photo_url: "",
+      intro_text: "",
+      native_language: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
       <div className="form-div">
-        <h1>STUDENT STUDENT STUDENT</h1>
-        <form>
+        <h1>Student Registration</h1>
+        <h5>
+          Ready to start learning? Please complete this registration form to get
+          started today!
+        </h5>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-row">
             <div className="form-group col-md-6">
-              <label for="inputEmail4">Full Name</label>
+              <label>Full Name</label>
               <input
                 type="text"
                 className="form-control"
-                id="input-fullname"
                 placeholder="Full Name"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
               />
             </div>
             <div className="form-group col-md-6">
-              <label for="inputPassword4">Password</label>
+              <label>Choose a username</label>
               <input
-                type="password"
+                type="text"
                 className="form-control"
-                id="inputPassword4"
-                placeholder="Password"
+                placeholder="Username"
+                value={this.state.username}
+                name="username"
+                onChange={this.handleChange}
               />
             </div>
           </div>
           <div className="form-row">
             <div className="form-group col-md-6">
-              <label for="inputEmail4">Email</label>
+              <label>Email address</label>
               <input
                 type="email"
                 className="form-control"
-                id="inputEmail4"
                 placeholder="Email"
+                value={this.state.email}
+                name="email"
+                onChange={this.handleChange}
               />
             </div>
             <div className="form-group col-md-6">
-              <label for="inputPassword4">Password</label>
+              <label>Choose a password</label>
               <input
                 type="password"
                 className="form-control"
-                id="inputPassword4"
                 placeholder="Password"
+                value={this.state.password_digest}
+                name="password_digest"
+                onChange={this.handleChange}
               />
             </div>
-          </div>
-          <div className="form-group">
-            <label for="inputAddress">Address</label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputAddress"
-              placeholder="1234 Main St"
-            />
-          </div>
-          <div className="form-group">
-            <label for="inputAddress2">Address 2</label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputAddress2"
-              placeholder="Apartment, studio, or floor"
-            />
           </div>
           <div className="form-row">
             <div className="form-group col-md-6">
-              <label for="inputCity">City</label>
-              <input type="text" className="form-control" id="inputCity" />
-            </div>
-            <div className="form-group col-md-4">
-              <label for="inputState">State</label>
-              <select id="inputState" className="form-control">
-                <option selected>Choose...</option>
-                <option>...</option>
-              </select>
-            </div>
-            <div className="form-group col-md-2">
-              <label for="inputZip">Zip</label>
-              <input type="text" className="form-control" id="inputZip" />
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="form-check">
+              <label>Skype ID</label>
               <input
-                className="form-check-input"
-                type="checkbox"
-                id="gridCheck"
+                type="text"
+                className="form-control"
+                placeholder="Skype"
+                vaclue={this.state.skype_id}
+                name="skype_id"
+                onChange={this.handleChange}
               />
-              {/* TODO */}
-              <label className="form-check-label" for="gridCheck">
-                I agree to the Terms & Conditions
-              </label>
+            </div>
+            <div className="form-group col-md-6">
+              <label>Enter a photo for your profile</label>
+              <input
+                type="url"
+                className="form-control"
+                placeholder="Default photo URL"
+                value={this.state.photo_url}
+                name="photo_url"
+                onChange={this.handleChange}
+              />
             </div>
           </div>
-          <button type="submit" className="btn btn-primary">
+          <div className="form-row">
+            <div className="form-group col-md-3">
+              <label>Native Language</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                value={this.state.native_language}
+                name="native_language"
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-group col-md-9">
+              <label>Tell us something about you</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                value={this.state.intro_text}
+                name="intro_text"
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+          <button type="submit" className="btn btn-secondary">
             Sign up
           </button>
         </form>
