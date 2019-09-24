8fff70e98e5c54be1eced764dd3b567eff13224f
Amanda Treutler <amandathedev@gmail.com>
2019-09-23 19:19:56 -0500

    fixes social media buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,19 +10,19 @@ const Footer = props => {
     // 	SOURCE https://stackoverflow.com/questions/33517524/how-to-add-the-footer-with-social-icons-in-bootstrap/33518069
     <footer className="footer fixed-bottom">
       <div className="container text-center">
-        <a href="#">
+        <a href="http://www.facebook.com">
           <i className="fa fa-facebook"></i>
         </a>
-        <a href="#">
+        <a href="http://www.twitter.com">
           <i className="fa fa-twitter"></i>
         </a>
-        <a href="#">
+        <a href="mailto:contact.nativespeak@gmail.com">
           <i className="far fa-envelope"></i>
         </a>
-        <a href="#">
+        <a href="http://www.instagram.com">
           <i className="fab fa-instagram"></i>
         </a>
-        <a href="#">
+        <a href="http://www.skype.com">
           <i className="fa fa-skype"></i>
         </a>
       </div>
