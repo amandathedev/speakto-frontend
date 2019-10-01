9a5b684bd75287c3775f6ab6d2e8be5ce00df999
Amanda Treutler <amandathedev@gmail.com>
2019-10-01 13:24:59 -0500

    sorts lessons

--- src/components/StudentComponents/LessonsList.js
+++ src/components/StudentComponents/LessonsList.js
@@ -18,16 +18,29 @@ export default class LessonsList extends Component {
       }
     })
       .then(resp => resp.json())
-      .then(lessons =>
+      .then(lessons => {
+        lessons.map(lesson => {
+          lesson.timeslot.realdate = new Date(lesson.timeslot.realdate);
+          return lesson;
+        });
         this.setState({
           lessons
-        })
-      );
+        });
+      });
   }
 
+  sortDates = () => {
+    return this.state.lessons.sort(function(a, b) {
+      // Turn your strings into dates, and then subtract them
+      // to get a value that is either negative, positive, or zero.
+      return new Date(a.timeslot.realdate) - new Date(b.timeslot.realdate);
+    });
+  };
+
   renderTableData = () => {
+    // map over sorted array from separate function
     return this.state.lessons.length ? (
-      this.state.lessons.map(lesson => {
+      this.sortDates().map(lesson => {
         return (
           <tr>
             <th scope="row">{lesson.id}</th>
@@ -36,6 +49,13 @@ export default class LessonsList extends Component {
               {lesson.timeslot.month_name} {lesson.timeslot.date}
             </td>
             <td>{lesson.timeslot.hour}:00</td>
+            {lesson.timeslot.realdate < this.state.current_time ? (
+              <td>
+                <button>review pls</button>
+              </td>
+            ) : (
+              <td>:)</td>
+            )}
           </tr>
         );
       })
@@ -44,16 +64,23 @@ export default class LessonsList extends Component {
     );
   };
 
+  alreadyHappened = () => {
+    return this.state.lessons.map(lesson => {
+      {
+        return lesson.timeslot.realdate > this.state.current_time
+          ? true
+          : false;
+      }
+    });
+  };
+
   render() {
+    this.alreadyHappened();
     return (
       <div>
         <h1 className="lesson-h1">Lessons</h1>
         <br></br>
-        <div className="col-md-12 student-button-group">
-          {/* <button type="button" className="btn student-buttons btn-lg">
-            Lesson History
-          </button> */}
-        </div>
+        <div className="col-md-12 student-button-group"></div>
         <table className="table table-striped lesson-table">
           <thead>
             <tr>
